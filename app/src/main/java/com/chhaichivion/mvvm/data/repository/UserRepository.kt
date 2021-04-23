b435d774cef9272961f18343e012977f7419464a
Chhai Chivon <chivonchhai.it@gmail.com>
2021-04-23 16:55:56 +0700

    Add structure mvvm

---
+++ app/src/main/java/com/chhaichivion/mvvm/data/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.chhaichivion.mvvm.data.repository
+
+
+/**
+ * Copyright (c) PRASAC MFI, Ltd. All rights reserved. (https://www.prasac.com.kh/)
+ * Author	: Chhai Chivon (chivon.chhai@prasac.com.kh) on 4/23/21.
+ * Position : Senior Application Development Officer
+ */
+class UserRepository {
+}
--- app/src/main/java/com/chhaichivion/mvvm/ui/MainActivity.kt
+++ app/src/main/java/com/chhaichivion/mvvm/ui/MainActivity.kt
@@ -6,6 +6,9 @@ import com.chhaichivion.mvvm.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //https://github.com/IsaiasCuvula/weather_app
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
