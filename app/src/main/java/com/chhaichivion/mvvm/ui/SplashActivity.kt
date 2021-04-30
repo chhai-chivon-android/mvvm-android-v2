ff25193c125ff65ce606f10c11128b74c4f39b9e
Chivon Chhai <chivonchhai.it@gmail.com>
2021-05-01 00:15:31 +0700

    Update splash screen

--- app/src/main/java/com/chhaichivion/mvvm/ui/SplashActivity.kt
+++ app/src/main/java/com/chhaichivion/mvvm/ui/SplashActivity.kt
@@ -1,12 +1,23 @@
 package com.chhaichivion.mvvm.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.chhaichivion.mvvm.R
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val buttonStart = findViewById<Button>(R.id.btnStart)
+        buttonStart.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
